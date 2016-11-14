050ed45f1b81611228e9f338a5c8117233f3f4a1
aebmknrl <alienriquebm@gmail.com>
2016-11-14 17:30:44 -0400

    First steps for edit a row
    Edit on row implemented

--- js/autorepuestosApp.js
+++ js/autorepuestosApp.js
@@ -1,20 +1,20 @@
 angular
-    .module('autorepuestosApp', ['ui.router','cgBusy','LocalStorageModule','ui.bootstrap'])
+    .module('autorepuestosApp', ['ui.router', 'cgBusy', 'LocalStorageModule', 'ui.bootstrap', 'ngMessages'])
     .filter('range', function() {
-      return function(input, total) {
-        total = parseInt(total);
+        return function(input, total) {
+            total = parseInt(total);
 
-        for (var i=0; i<total; i++) {
-          input.push(i);
-        }
+            for (var i = 0; i < total; i++) {
+                input.push(i);
+            }
 
-        return input;
-      };
+            return input;
+        };
     })
-    .config(function (localStorageServiceProvider) {
-      localStorageServiceProvider
-        .setPrefix('autorepuestos')
-        .setStorageType('sessionStorage');
+    .config(function(localStorageServiceProvider) {
+        localStorageServiceProvider
+            .setPrefix('autorepuestos')
+            .setStorageType('sessionStorage');
     })
     .config(function($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.otherwise('/');
@@ -51,7 +51,7 @@ angular
             $state.go("marcas");
         };
     }])
-    .controller('marcasController', ['$scope', '$state', '$http' ,'storageService','endpointApiURL', function($scope, $state, $http,storageService,endpointApiURL) {
+    .controller('marcasController', ['$scope', '$state', '$http', 'storageService', 'endpointApiURL', function($scope, $state, $http, storageService, endpointApiURL) {
 
         var marcas_controller = this;
         marcas_controller.filter = "";
@@ -59,51 +59,49 @@ angular
         $scope.QtyPageTables = storageService.getQtyPageTables();
 
 
-        marcas_controller.ChangeQtyPagesTables = function(Qty,searchText){
+        marcas_controller.ChangeQtyPagesTables = function(Qty, searchText) {
             storageService.setQtyPageTables(Qty);
             $scope.QtyPageTables = storageService.getQtyPageTables();
-            marcas_controller.getMarcas(Qty,1,searchText);
+            marcas_controller.getMarcas(Qty, 1, searchText);
         }
 
-        marcas_controller.getMarcas =  function(limit,page,searchText){
-
-             if (searchText !== undefined){
+        marcas_controller.getMarcas = function(limit, page, searchText) {
+            if (searchText !== undefined) {
                 if (searchText !== "") {
                     var url = endpointApiURL.url + "/marca/" + limit + "/" + page + "/" + searchText;
                 } else {
                     var url = endpointApiURL.url + "/marca/" + limit + "/" + page;
                 }
-                    
-                } else {
-                    var url = endpointApiURL.url + "/marca/" + limit + "/" + page;
-                }
-            
+
+            } else {
+                var url = endpointApiURL.url + "/marca/" + limit + "/" + page;
+            }
             $scope.getMarcasPromise = $http.get(url)
-                .then(function(response){
+                .then(function(response) {
                     //console.log(response.data);
                     marcas_controller.allLoad = false;
                     marcas_controller.CurrentPage = page;
                     marcas_controller.marcas = response.data.marcas;
                     marcas_controller.totalMarcas = response.data.totalMarcas;
-                    marcas_controller.totalMarcasReturned = response.data.totalMarcasReturned;          
+                    marcas_controller.totalMarcasReturned = response.data.totalMarcasReturned;
                     if ((limit == 'todos') || (limit == 'Todos')) {
                         marcas_controller.totalPages = 1;
                         marcas_controller.actualRange = "Mostrando todos los registros (" + marcas_controller.totalMarcasReturned + ")";
                     } else {
                         marcas_controller.totalPages = Math.ceil(marcas_controller.totalMarcas / limit);
-                        marcas_controller.pageFrom = (limit * page) - (limit - 1); 
-                        marcas_controller.pageTo = (marcas_controller.pageFrom + marcas_controller.totalMarcasReturned) - 1; 
+                        marcas_controller.pageFrom = (limit * page) - (limit - 1);
+                        marcas_controller.pageTo = (marcas_controller.pageFrom + marcas_controller.totalMarcasReturned) - 1;
                         marcas_controller.actualRange = "Mostrando registros " + marcas_controller.pageFrom + " a " + marcas_controller.pageTo + " de " + marcas_controller.totalMarcas
- 
+
                     }
                     marcas_controller.allLoad = true;
                     // Only for tests
                     //console.log("Total paginas: " + marcas_controller.totalPages);
                     //console.log("Página actual: " + marcas_controller.CurrentPage);  
                     //console.log("Registro " + marcas_controller.pageFrom + " a " + marcas_controller.pageTo + " de " + marcas_controller.totalMarcas);               
-                    
+
                 })
-                .catch(function(error){
+                .catch(function(error) {
                     console.log(error);
                     if (error.status == '412') {
                         console.log('Error obteniendo datos: ' + error.data.error);
@@ -111,36 +109,44 @@ angular
                 });
         }
 
-        marcas_controller.setPage = function(page){
-            marcas_controller.getMarcas($scope.QtyPageTables,page);
+        marcas_controller.setPage = function(page) {
+            marcas_controller.getMarcas($scope.QtyPageTables, page);
         }
+
+        marcas_controller.selectForAction = function(nombre, observaciones, id) {
+            marcas_controller.selectedItem = {
+                nombre: nombre,
+                observaciones: observaciones,
+                id: id
+            }
+        }
+
 
         // The default value on load controller:
-        marcas_controller.getMarcas($scope.QtyPageTables,1);
+        marcas_controller.getMarcas($scope.QtyPageTables, 1);
 
     }])
-    .service('storageService', ['localStorageService',function(localStorageService){
-        this.setQtyPageTables = function(qty = '10'){
-            localStorageService.set('QtyPageTables', qty); 
+    .service('storageService', ['localStorageService', function(localStorageService) {
+        this.setQtyPageTables = function(qty = '10') {
+            localStorageService.set('QtyPageTables', qty);
         }
 
-        this.getQtyPageTables = function(){
+        this.getQtyPageTables = function() {
             var actualValue = localStorageService.get('QtyPageTables');
             if (actualValue != undefined) {
                 return actualValue;
-            }
-            else {
-                this.setQtyPageTables('10'); 
+            } else {
+                this.setQtyPageTables('10');
                 actualValue = localStorageService.get('QtyPageTables');
                 return actualValue;
             }
         }
     }])
-    .directive('onEnter', function () {
-        return function (scope, element, attrs) {
-            element.bind("keydown keypress", function (event) {
-                if(event.which === 13) {
-                    scope.$apply(function (){
+    .directive('onEnter', function() {
+        return function(scope, element, attrs) {
+            element.bind("keydown keypress", function(event) {
+                if (event.which === 13) {
+                    scope.$apply(function() {
                         scope.$eval(attrs.onEnter);
                     });
                     event.preventDefault();
