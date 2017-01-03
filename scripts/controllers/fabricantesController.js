d0bb25acd94fff522230a3b7eb6fa466108ee151
aebmknrl <alienriquebm@gmail.com>
2017-01-02 22:33:56 -0400

    Adding DatePicket to Fabricantes

--- scripts/controllers/fabricantesController.js
+++ scripts/controllers/fabricantesController.js
@@ -201,4 +201,46 @@ angular
 
         // Get the countrys 
         fabricantesc.paises = countryService.getCountrys();
+
+        // Datepicket Options -----------------------------------------------------
+        $scope.options = {
+            customClass: getDayClass,
+            minDate: new Date(),
+            showWeeks: true
+        };
+
+        // Datepicker initializations
+        var tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        var afterTomorrow = new Date(tomorrow);
+        afterTomorrow.setDate(tomorrow.getDate() + 1);
+        $scope.events = [
+            {
+            date: tomorrow,
+            status: 'full'
+            },
+            {
+            date: afterTomorrow,
+            status: 'partially'
+            }
+        ];
+
+        function getDayClass(data) {
+            var date = data.date,
+            mode = data.mode;
+            if (mode === 'day') {
+            var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+            for (var i = 0; i < $scope.events.length; i++) {
+                var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+                if (dayToCheck === currentDay) {
+                return $scope.events[i].status;
+                }
+            }
+            }
+
+            return '';
+        }        
+
     }])
