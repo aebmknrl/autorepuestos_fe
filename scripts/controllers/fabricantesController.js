ed402c11b67c1d4e1509f28b2f761ff8fb355161
aebmknrl <alienriquebm@gmail.com>
2017-01-18 07:26:43 -0400

    Still working on datepicket

--- scripts/controllers/fabricantesController.js
+++ scripts/controllers/fabricantesController.js
@@ -203,44 +203,100 @@ angular
         fabricantesc.paises = countryService.getCountrys();
 
         // Datepicket Options -----------------------------------------------------
-        $scope.options = {
-            customClass: getDayClass,
-            minDate: new Date(),
-            showWeeks: true
-        };
 
-        // Datepicker initializations
-        var tomorrow = new Date();
-        tomorrow.setDate(tomorrow.getDate() + 1);
-        var afterTomorrow = new Date(tomorrow);
-        afterTomorrow.setDate(tomorrow.getDate() + 1);
-        $scope.events = [
-            {
-            date: tomorrow,
-            status: 'full'
-            },
-            {
-            date: afterTomorrow,
-            status: 'partially'
-            }
-        ];
+$scope.today = function() {
+    $scope.dt = new Date();
+  };
+  
+  $scope.today();
 
-        function getDayClass(data) {
-            var date = data.date,
-            mode = data.mode;
-            if (mode === 'day') {
-            var dayToCheck = new Date(date).setHours(0,0,0,0);
+  $scope.clear = function() {
+    $scope.dt = null;
+  };
 
-            for (var i = 0; i < $scope.events.length; i++) {
-                var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+  $scope.inlineOptions = {
+    customClass: getDayClass,
+    minDate: new Date(),
+    showWeeks: true
+  };
 
-                if (dayToCheck === currentDay) {
-                return $scope.events[i].status;
-                }
-            }
-            }
+  $scope.dateOptions = {
+    dateDisabled: disabled,
+    formatYear: 'yy',
+    maxDate: new Date(2020, 5, 22),
+    minDate: new Date(),
+    startingDay: 1
+  };
 
-            return '';
-        }        
+  // Disable weekend selection
+  function disabled(data) {
+    var date = data.date,
+      mode = data.mode;
+    return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+  }
+
+  $scope.toggleMin = function() {
+    $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
+    $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
+  };
+
+  $scope.toggleMin();
+
+  $scope.open1 = function() {
+    $scope.popup1.opened = true;
+  };
+
+  $scope.open2 = function() {
+    $scope.popup2.opened = true;
+  };
+
+  $scope.setDate = function(year, month, day) {
+    $scope.dt = new Date(day, month, year);
+  };
+
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
+  $scope.altInputFormats = ['d!/M!/yyyy'];
+
+  $scope.popup1 = {
+    opened: false
+  };
+
+  $scope.popup2 = {
+    opened: false
+  };
+
+  var tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  var afterTomorrow = new Date();
+  afterTomorrow.setDate(tomorrow.getDate() + 1);
+  $scope.events = [
+    {
+      date: tomorrow,
+      status: 'full'
+    },
+    {
+      date: afterTomorrow,
+      status: 'partially'
+    }
+  ];
+
+  function getDayClass(data) {
+    var date = data.date,
+      mode = data.mode;
+    if (mode === 'day') {
+      var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+      for (var i = 0; i < $scope.events.length; i++) {
+        var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+        if (dayToCheck === currentDay) {
+          return $scope.events[i].status;
+        }
+      }
+    }
+
+    return '';
+  }  
 
     }])
