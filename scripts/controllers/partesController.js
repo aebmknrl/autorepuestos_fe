30fd634c8e8846f2e1107a035e5f1afb8c716d4b
aebmknrl <alienriquebm@gmail.com>
2017-02-02 00:26:43 -0400

    First Steps on Modal for Partes Controller

--- scripts/controllers/partesController.js
+++ scripts/controllers/partesController.js
@@ -1,6 +1,6 @@
 angular
     .module('autorepuestosApp')
-    .controller('partesController', ['$scope', '$state', '$http', 'storageService', 'endpointApiURL', 'ngToast', '$uibModal', '$log', '$confirm', '$rootScope','toastMsgService', function ($scope, $state, $http, storageService, endpointApiURL, ngToast, $uibModal, $log, $confirm, $rootScope,toastMsgService) {
+    .controller('partesController', ['$scope', '$state', '$http', 'storageService', 'endpointApiURL', 'ngToast', '$uibModal', '$log', '$confirm', '$rootScope','toastMsgService','$document', function ($scope, $state, $http, storageService, endpointApiURL, ngToast, $uibModal, $log, $confirm, $rootScope,toastMsgService,$document) {
         // Set the username for the app
         $rootScope.username = storageService.getUserData('username');
         $rootScope.userrole = storageService.getUserData('role');
@@ -98,5 +98,249 @@ angular
                 });
         }
 
+        // Change the items per page
+        partesc.ChangeQtyPagesTables = function (Qty, searchText) {
+            storageService.setQtyPageTables(Qty);
+            $scope.QtyPageTables = storageService.getQtyPageTables();
+            partesc.getFabricantes(Qty, 1, searchText);
+        }
 
+        // Copy temporally item data for edit
+        // This method appears not apply to Partes
+        partesc.copyRowData = function (data) {
+            // partesc.selectedItem.nombre = data;
+        }
+
+        // Add item
+        partesc.addParte = function (nombre,nombrepieza,nombreinventario, paramazon, codigo, codigoupc,grupo,subgrupo,largo,ancho,espesor,peso,caracteristicas,observacion,kit,equivalencia,conjunto,fabricanteid) {
+
+            url = endpointApiURL.url + "/parte/add";
+            $scope.FabricantesPromise = $http.post(
+                    url, {
+                        "nombre": nombre,
+                        "nombrepieza": nombrepieza,
+                        "nombreinventario": nombreinventario,
+                        "paramazon": paramazon,
+                        "codigo": codigo,
+                        "codigoupc": codigoupc,
+                        "grupo": grupo,
+                        "subgrupo": subgrupo,
+                        "largo": largo,
+                        "ancho": ancho,
+                        "espesor": espesor,
+                        "peso": peso,
+                        "caracteristicas": caracteristicas,
+                        "observacion": observacion,
+                        "kit": kit,
+                        "equivalencia": equivalencia,
+                        "conjunto": conjunto,
+                        "fabricanteid": fabricanteid
+                    }
+                )
+                .then(function (response) {
+                    //console.log(response.data.fabricantes);
+                    partesc.getFabricantes($scope.QtyPagesSelected, partesc.CurrentPage, partesc.searchText);
+                    ngToast.create({
+                        className: 'info',
+                        content: '<span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> Registro agregado: <strong>' + response.data.parte[0].parte + '</strong>'
+                    });
+                    partesc.selectedItem.id = 0;                   
+                    partesc.newItem = {
+                        nombre: '',
+                        nombrepieza: '',
+                        nombreinventario: '',
+                        paramazon: '',
+                        codigo: '',
+                        codigoupc: '',
+                        grupo: '',
+                        subgrupo: '',
+                        largo: '',
+                        ancho: '',
+                        espesor: '',
+                        peso: '',
+                        caracteristicas: '',
+                        observacion: '',
+                        kit: '',
+                        equivalencia: '',
+                        conjunto: '',
+                        fabricanteid: ''
+                    };
+
+                    $scope.newParteForm.nombre.$touched = false;
+                    $scope.newParteForm.nombrepieza.$touched = false;
+                    partesc.isAddNewParte = false;
+                })
+                .catch(function (error) {
+                    //console.log(error);
+                   toastMsgService.showMsg('Error cód.: ' + error.data.error.code + ' Mensaje: ' + error.data.error.message + ': ' + error.data.error.exception[0].message, 'danger', 10000);
+                });
+        }
+
+
+        // Update item
+        partesc.updateParte = function (nombre,nombrepieza,nombreinventario, paramazon, codigo, codigoupc,grupo,subgrupo,largo,ancho,espesor,peso,caracteristicas,observacion,kit,equivalencia,conjunto,fabricanteid) {
+            if (!id || !nombre || !nombrepieza) {
+                return false;
+            }
+            url = endpointApiURL.url + "/parte/edit/" + id;
+            $scope.PartesPromise = $http.post(
+                    url, {
+                        "nombre": nombre,
+                        "nombrepieza": nombrepieza,
+                        "nombreinventario": nombreinventario,
+                        "paramazon": paramazon,
+                        "codigo": codigo,
+                        "codigoupc": codigoupc,
+                        "grupo": grupo,
+                        "subgrupo": subgrupo,
+                        "largo": largo,
+                        "ancho": ancho,
+                        "espesor": espesor,
+                        "peso": peso,
+                        "caracteristicas": caracteristicas,
+                        "observacion": observacion,
+                        "kit": kit,
+                        "equivalencia": equivalencia,
+                        "conjunto": conjunto,
+                        "fabricanteid": fabricanteid
+                    }
+                )
+                .then(function (response) {
+                    //console.log(response.data);
+                    partesc.getPartes($scope.QtyPagesSelected, partesc.CurrentPage, partesc.searchText);
+                    ngToast.create({
+                        className: 'info',
+                        content: '<span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> Cambios guardados'
+                    });
+                    partesc.selectedItem.id = 0;
+                })
+                .catch(function (error) {
+                    //console.log(error);
+                   toastMsgService.showMsg('Error cód.: ' + error.data.error.code + ' Mensaje: ' + error.data.error.message + ': ' + error.data.error.exception[0].message, 'danger', 10000);
+                });
+        }
+
+
+// Get items
+        partesc.getPartes = function (limit, page, searchText) {
+
+            if (searchText !== undefined) {
+                if (searchText !== "") {
+                    var url = endpointApiURL.url + "/parte/" + limit + "/" + page + "/" + searchText;
+                } else {
+                    var url = endpointApiURL.url + "/parte/" + limit + "/" + page;
+                }
+
+            } else {
+                var url = endpointApiURL.url + "/parte/" + limit + "/" + page;
+            }
+            //console.log('The parameters send are: URL=' + url);
+            $scope.PartesPromise = $http.get(url)
+                .then(function (response) {
+                    //console.log(response.data.fabricantes);
+                    partesc.allLoad = false;
+                    partesc.CurrentPage = page;
+                    partesc.partes = response.data.partes;
+                    partesc.totalPartes = response.data.totalPartes;
+                    partesc.totalPartesReturned = response.data.totalPartesReturned;
+                    if ((limit == 'todos') || (limit == 'Todos')) {
+                        partesc.totalPages = 1;
+                        partesc.actualRange = "Mostrando todos los registros (" + partesc.totalPartesReturned + ")";
+                    } else {
+                        partesc.totalPages = Math.ceil(partesc.totalPartes / limit);
+                        partesc.pageFrom = (limit * page) - (limit - 1);
+                        partesc.pageTo = (partesc.pageFrom + partesc.totalPartesReturned) - 1;
+                        partesc.actualRange = "Mostrando registros " + partesc.pageFrom + " a " + partesc.pageTo + " de " + partesc.totalPartes
+
+                    };
+                    partesc.allLoad = true;
+
+                })
+                .catch(function (error) {
+                    //console.log(error);
+                   toastMsgService.showMsg('Error cód.: ' + error.data.error.code + ' Mensaje: ' + error.data.error.message + ': ' + error.data.error.exception[0].message, 'danger', 10000);
+                });
+        }
+          // Set page
+        partesc.setPage = function (page) {
+            partesc.getPartes($scope.QtyPageTables, page);
+        }
+
+        // The default value on load controller:
+        partesc.getPartes($scope.QtyPageTables, 1);
+
+
+        // Get the Marcas for Modelos entity
+        partesc.getFabricantes = function () {
+                var url = endpointApiURL.url + "/fabricante";
+                $scope.PartesPromise = $http.get(url)
+                    .then(function (response) {
+                        partesc.fabricantes = response.data;
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                        if (error.status == '412') {
+                            console.log('Error obteniendo datos: ' + error.data.error);
+                        }
+                    });
+            }
+
+        // Call get Fabricantes to populate the select
+        partesc.getFabricantes();
+
+
+
+    // Modal
+    partesc.items = ['item1', 'item2', 'item3'];
+    partesc.animationsEnabled = true;
+    partesc.selected = {
+        item: partesc.items[0]
+    };
+
+    partesc.open = function () {
+      var modal =  $uibModal.open({
+            animation: partesc.animationsEnabled,
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: 'views/partesView.html',
+            controller: 'partesController',
+            controllerAs: 'partesc',
+            size: 'sm',
+            appendTo:  undefined,
+            resolve: {
+                items: function () {
+                return partesc.items;
+                }
+            }
+        })
+    }
+
+    partesc.ok = function () {
+        modal.close(partesc.selected.item);
+    };
+
+    partesc.cancel = function () {
+        modal.dismiss('cancel');
+    };
+
+/*    partesc.open = function (size, parentSelector) {
+        var parentElem = parentSelector ? 
+        angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
+        var modalInstance = $uibModal.open({
+        animation: partesc.animationsEnabled,
+        ariaLabelledBy: 'modal-title',
+        ariaDescribedBy: 'modal-body',
+        templateUrl: 'myModalContent.html',
+        controller: 'modalInstanceController',
+        controllerAs: '$ctrl',
+        size: size,
+        appendTo: parentElem,
+        resolve: {
+            items: function () {
+            return partesc.items;
+            }
+        }
+        })
+    }
+*/
     }])
